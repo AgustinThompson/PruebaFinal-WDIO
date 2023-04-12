7ead9aae58dfc5d824c0cbf94039a61c4d584481
Agustin Thompson <htnitsuga@gmail.com>
2023-04-12 13:20:58 -0300

    correcciones en el test de opencart

--- tests/opencart.test_dos.js
+++ tests/opencart.test_dos.js
@@ -1,25 +1,11 @@
-import { addStep } from '@wdio/allure-reporter';
-import { expect } from 'chai';
 import HomePage from '../pages/home.page';
 import SearchPage from '../pages/search.page';
 
 
-describe('Carrito Dos', () => {
-
-  before(async () => {
-    await HomePage.abrir('/');
-  });
-
-  after(async () => {
-    await HomePage.cerrar();
-  });
-
-  it('Debería buscar iphone', async ()=> {
-    const articulo = 'iPhone';
-    addStep(`Buscar artículo "${articulo}"`);
-    await HomePage.buscar(articulo);
-    expect(await HomePage.obtenerTextoBusqueda()).to.equal(articulo);
-    expect(await SearchPage.obtenerNombreResultado()).to.equal(articulo);
-    addStep(`Se encontró el artículo "${articulo}"`);
-  });
+it('Debería buscar iphone', async ()=> {
+  await HomePage.abrir('/');
+  const articulo = 'iPhone';
+  await HomePage.buscar(articulo);
+  expect(await HomePage.obtenerTextoBusqueda()).to.equal(articulo);
+  expect(await SearchPage.obtenerNombreResultado()).to.equal(articulo);
 });
