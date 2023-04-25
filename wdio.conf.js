f22861fe0d078c65d27eb7427b8afeba14980bd9
Agustin Thompson <htnitsuga@gmail.com>
2023-04-25 19:32:56 -0300

    Reg.Visual

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 exports.config = {
     
     
@@ -118,7 +120,23 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['selenium-standalone'],
+    services: 
+    [
+        [
+          'image-comparison',
+          {
+            baselineFolder: path.join(process.cwd(), './visual-regression/baseline/'),
+            formatImageName: '{tag}-{logName}-{width}x{height}',
+            screenshotPath: path.join(process.cwd(), './visual-regression/'),
+            savePerInstance: true,
+            autoSaveBaseline: true,
+            blockOutStatusBar: true,
+            blockOutToolBar: true,
+            // más opciones...
+          },
+        ],
+        'selenium-standalone',
+      ],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
